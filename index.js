9245a89b6401a492dd5807b74ebc65991eef3945
sandrorukhadze <124590218+sandrorukhadze@users.noreply.github.com>
2024-01-12 23:36:30 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+/* 1
+let array2 = [14, 150, 'css', null, 'javascript', 25];
+let array2New = array2.map(function(obj){
+    if (array2New == Number)
+}) 
+*/
+
+
+/* 2 +
+let info= 'good day'
+let info2 = info.slice(5,8)
+
+console.log(info2);
+*/
+
+
+/* 3 +
+function func(String) {
+    return func.length
+}
+
+let result = func();
+console.log(result);
+*/
+
+
+/* 4 + 
+const question = prompt('რომელია საქართველოს დედაქალაქი ? ', 'ჩაწერეთ...');
+
+if(question.toLowerCase() === 'tbilisi') {
+  alert('პასუხი სწორია!');
+} else {
+  alert('პასუხი არასწორია!');
+}
+*/
+
+
+/* 5 +
+let fruits = ['apple', 'mango', 'avocado','kiwi'];
+fruits.splice(2,1,'strawberry')
+
+console.log(fruits);
+*/
+
+
+/* 6 + 
+let array = [5, 25, 89, 120, 36];
+
+array.push('javascript','python');
+array.unshift('html','css');
+array.shift();
+array.pop();
+
+console.log(array);
+console.log(array.length);
+*/
+
+
+/* 7 + 
+let fruits = ['ფორთოხალი', 'ბანანი', 'კივი'];
+fruits.push('ვაშლი','ანანასი')
+fruits.unshift('საზამთრო')
+fruits.splice(2,1,'მანგო')
+fruits.splice(0,1)
+fruits.splice(-1,1)
+console.log(fruits.length);
+console.log(fruits);
+*/
+
+
+/* 8 + 
+let array3 =[1, 2, 3, 4, 5];
+array3.splice(3,0,'a','b','c')
+
+console.log(array3);
+*/
+
+
+/* 9 + 
+array5 = [12, 25, 3, 6, 8, 14, 7, 23];
+
+let array5New = array5.map(function(item){
+  return item / 3
+})
+
+console.log(array5New);
+*/
+
+
+/* 10 + 
+let array7 =  [15, 100, 25, 10, 36]
+array7.splice(3,1)
+
+console.log(array7);
+*/
+
+
+/* 11 + 
+let array8 = [1, 2, 3 , 4 , 5]
+array8.splice(2,1,'three')
+
+console.log(array8);
+*/
